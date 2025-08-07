2bac9b3693952f3d463f25349fd4b8618554a049
Lethabo Matseke <lerickalc1x@gmail.com>
2025-08-07 14:00:57 +0200

    basic recap

---
+++ dictionaries.py
@@ -0,0 +1,10 @@
+#dictionaries
+
+fruit_dictionary = {'apple':0,'banana':1,'pear':2,'orange':3,'mango':4}
+print(fruit_dictionary['mango'])
+
+fruit_dictionary['grape'] = 5
+print(fruit_dictionary)
+
+fruit_dictionary['banana'] = 8
+print(fruit_dictionary)
